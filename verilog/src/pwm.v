e60d782a3261a5877f74595ae59492a3ad1df58e
Joshua Conner <joshua.lee.conner@gmail.com>
2020-02-02 00:24:09 -0700

    Made PWM module flexible to duty cycle data width

--- verilog/src/pwm.v
+++ verilog/src/pwm.v
@@ -1,9 +1,43 @@
-module pwm (
-  input i_clk,
-  input i_en,
-  output reg o_pwm
-);
+module pwm 
+  #(
+    parameter N = 8,
+    parameter PERIOD_CLK_COUNT = 2000000) //2000000*10ns=20ms
+  (
+    input i_clk,
+    input i_rst,
+    input i_en,
+    input [N-1:0] i_dutyCycle,
+    output reg o_pwm);
 
-  // Today's episode was amazing
-  // Definitely an S+ episode :)
+    integer dutyCycleCnt;
+
+  always @ (posedge i_clk) begin
+    if (i_rst == 1'b1) begin
+      dutyCycleCnt <= 1'b0;
+    end 
+    else begin
+      if (i_en == 1'b1) begin
+        if (dutyCycleCnt < PERIOD_CLK_COUNT-1) begin
+          dutyCycleCnt = dutyCycleCnt + 1;
+        end
+        else begin
+          dutyCycleCnt = 0;
+        end
+      end
+    end
+  end
+
+  always @ (posedge i_clk) begin
+    if (i_rst == 1'b1) begin
+      o_pwm <= 1'b0;
+    end
+    else begin
+      if (i_en == 1'b1) begin
+        o_pwm = (dutyCycleCnt < (i_dutyCycle * (PERIOD_CLK_COUNT >> N))) ? 1'b1 : 1'b0;
+      end
+      else begin
+        o_pwm = 1'b0;
+      end
+    end
+  end
 endmodule
