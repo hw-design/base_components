e19451d75873630976d92385c5f32457d435c3f6
Joshua Conner <joshua.lee.conner@gmail.com>
2020-02-08 12:38:04 -0700

    Added module comment header. Corrected port names to match coding style guidelines. Updated PWM module port names to match changes.

--- verilog/src/rgb_led_controller.v
+++ verilog/src/rgb_led_controller.v
@@ -1,37 +1,44 @@
+// Module Name: rgb_led_controller.v
+// Author     : Joshua Conner
+// Description: RGB LED controller
+// Notes      : Change PWM resolution with N parameter and PWM period with
+//              PERIOD_CLK_COUNT
+//              All three PWM modules share a common resolution and PWM period
+
 module rgb_led_controller 
   # (
     parameter N = 8,
     parameter PERIOD_CLK_COUNT = 2000000)
   (
-    input i_clk,
-    input i_rst,
-    input i_en,
-    input [N-1:0] i_ledRDutyCycle,
-    input [N-1:0] i_ledGDutyCycle,
-    input [N-1:0] i_ledBDutyCycle,
-    output o_ledRPwm,
-    output o_ledGPwm,
-    output o_ledBPwm);
+    input clkIn,
+    input rstIn,
+    input enIn,
+    input [N-1:0] ledRDutyCycleIn,
+    input [N-1:0] ledGDutyCycleIn,
+    input [N-1:0] ledBDutyCycleIn,
+    output ledRPwmOut,
+    output ledGPwmOut,
+    output ledBPwmOut);
 
     pwm #(.N(N), .PERIOD_CLK_COUNT(PERIOD_CLK_COUNT)) LED_R (
-      .i_clk(i_clk),
-      .i_rst(i_rst),
-      .i_en(i_en),
-      .i_dutyCycle(i_ledRDutyCycle),
-      .o_pwm(o_ledRPwm));
+      .clkIn(clkIn),
+      .rstIn(rstIn),
+      .enIn(enIn),
+      .dutyCycleIn(ledRDutyCycleIn),
+      .pwmOut(ledRPwmOut));
 
     pwm #(.N(N), .PERIOD_CLK_COUNT(PERIOD_CLK_COUNT)) LED_G (
-      .i_clk(i_clk),
-      .i_rst(i_rst),
-      .i_en(i_en),
-      .i_dutyCycle(i_ledGDutyCycle),
-      .o_pwm(o_ledGPwm));
+      .clkIn(clkIn),
+      .rstIn(rstIn),
+      .enIn(enIn),
+      .dutyCycleIn(ledGDutyCycleIn),
+      .pwmOut(ledGPwmOut));
 
     pwm #(.N(N), .PERIOD_CLK_COUNT(PERIOD_CLK_COUNT)) LED_B (
-      .i_clk(i_clk),
-      .i_rst(i_rst),
-      .i_en(i_en),
-      .i_dutyCycle(i_ledBDutyCycle),
-      .o_pwm(o_ledBPwm));
+      .clkIn(clkIn),
+      .rstIn(rstIn),
+      .enIn(enIn),
+      .dutyCycleIn(ledBDutyCycleIn),
+      .pwmOut(ledBPwmOut));
 
 endmodule
